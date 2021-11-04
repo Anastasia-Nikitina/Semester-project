917bd30449dba12bb1535310d6f0d5156dc9a2fd
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2021-11-05 01:25:57 +0300

    fixed linear regression

--- practice.py
+++ practice.py
@@ -3,49 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg as lng
 
-data = pd.read_csv("./Advertising.csv")
-X = data["TV"].values.reshape(-1, 1)
-ones = np.ones([X.shape[0], 1])
-X = np.concatenate([ones, X], 1)
-Y = data["sales"].values.reshape(-1, 1)
 
+data = pd.read_csv("./dataset.csv")
+
+X = data.iloc[:, 0]
+Y = data.iloc[:, 1]
+
+theta_0 = 0
+theta_1 = 0
 alpha = 0.0001
+n = len(X)
 
-theta = np.array([0, 0])
-
-def hyp (X, theta):
-    m = X * theta
-    h = (m[:, 0]).reshape(-1, 1) + (m[:, 1]).reshape(-1, 1)
-    return h
-
-def function_of_cost (X, Y, theta):
-    sq_of_dif = np.power(hyp (X, theta) - Y, 2)
-    print (np.sum (sq_of_dif) / (2 * len(sq_of_dif)))
-
-def gradient_descent(alpha, theta, X, Y):
-    h = hyp (X, theta)
-    theta0 = (alpha/len (X)) * np.sum(h - Y)
-    theta1 = (alpha/len (X)) * np.sum((h - Y) * X[:, 1].reshape(-1, 1))
-    theta = theta - np.array([theta0, theta1])
-    return theta
+def hyp(X, theta_0, theta_1):
+    return(X*theta_1 + theta_0)
 
 
-theta_ = gradient_descent (alpha, theta, X, Y)
-X_ = ((X[:, 1]).reshape(-1, 1))
-Y_ = hyp (X, theta_ )
+for i in range(10000):
+    H = hyp(X, theta_0, theta_1)
+    theta_0 -= (alpha/n) * np.sum(H - Y)
+    theta_1 -= (alpha/n) * np.sum(X * (H - Y))
 
-plt.plot(X_, Y_)
-plt.scatter(
-    data['TV'],
-    data['sales'],
-    c = 'purple'
-    )
-plt.xlabel("Money spent on TV ads ($)")
-plt.ylabel("Sales ($)")
+Y_ = hyp(X, theta_0, theta_1) 
+plt.plot([min(X), max(X)], [min(X)*theta_1 + theta_0, max(X)*theta_1 + theta_0], color='blue')
+plt.scatter(X, Y, c = 'purple')
 plt.show
-
-
-
 
 
 
